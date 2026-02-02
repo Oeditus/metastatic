a85c6518ef6161b53f62b9935d8ef0477987e81e
Aleksei Matiushkin <am@ambment.cat>
2026-02-02 11:24:34 +0100

    :paperclip: version bump

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Metastatic.MixProject do
   use Mix.Project
 
   @app :metastatic
-  @version "0.6.1"
+  @version "0.7.0"
   @source_url "https://github.com/Oeditus/metastatic"
 
   def project do
